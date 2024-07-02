7911cc0c37fe9d4f3448225f6578719239e4faf4
Ivan Svyatykh <Zlativansvyat@mail.ru>
2024-07-02 18:51:37 +0500

    добавил получение категории через гс

--- Files/handlers/voice/voice.py
+++ Files/handlers/voice/voice.py
@@ -15,37 +15,37 @@ from Files.handlers.report.illegal_actions_3 import illegal_actions
 from Files.handlers.report.road_block_4 import road_block
 from Files.handlers.report.road_deficiencies_2 import road_deficiencies
 from Files.handlers.report.traffic_accident_1 import traffic_accident
+from Files.request import get_audio_label
 
 router = Router()
 
-with open(r'../recurses/text_for_message/data_for_mess.json',
-          'r') as data_for_mess:
+with open(r"../recurses/text_for_message/data_for_mess.json", "r") as data_for_mess:
     mes_data = load(data_for_mess)
 
-with open(r'../recurses/text_for_message/callback_data.json',
-          'r') as callback_mes_data:
+with open(r"../recurses/text_for_message/callback_data.json", "r") as callback_mes_data:
     callback_data = load(callback_mes_data)
 
 
-@router.callback_query(F.data == 'voice', States.advertisements)
+@router.callback_query(F.data == "voice", States.advertisements)
 async def voice_requirements(callback: types.CallbackQuery, state: FSMContext):
-    await callback.message.answer(text=mes_data['voice_requirements'])
+    await callback.message.answer(text=mes_data["voice_requirements"])
     await state.set_state(States.input_voice)
 
 
 @router.message(F.voice, States.input_voice)
 async def voice_processing(message: types.Message, state: FSMContext, bot: Bot):
-    await bot.download(message.voice, destination=f'{message.voice.file_id}.ogg')
-    await state.update_data({"file_name": f'{message.voice.file_id}.ogg'})
+    file_path = f"{message.voice.file_id}.ogg"
+    await bot.download(message.voice, destination=file_path)
+    await state.update_data({"file_name": file_path})
 
     # запрос для отправки гс на ии
-    skip_information = ['АЗС']  # полученный ответ
+    skip_information = await get_audio_label(file_path)  # полученный ответ
     await state.update_data({"to_do": skip_information[0]})
 
-    text = f'Вы хотите перейти к действию под названием: {skip_information[0]}. Верно?'
+    text = f"Вы хотите перейти к действию под названием: {skip_information[0]}. Верно?"
     markup = InlineKeyboardBuilder()
-    btn_1 = types.InlineKeyboardButton(text='Да', callback_data='voice_good')
-    btn_2 = types.InlineKeyboardButton(text='Нет', callback_data='voice_bad')
+    btn_1 = types.InlineKeyboardButton(text="Да", callback_data="voice_good")
+    btn_2 = types.InlineKeyboardButton(text="Нет", callback_data="voice_bad")
     markup.add(btn_1, btn_2)
     markup.adjust(2)
 
@@ -53,78 +53,51 @@ async def voice_processing(message: types.Message, state: FSMContext, bot: Bot):
     await state.set_state(States.voice_right_or_not)
 
 
-@router.callback_query(F.data == 'voice_bad', States.voice_right_or_not)
+@router.callback_query(F.data == "voice_bad", States.voice_right_or_not)
 async def voice_bad(callback: types.CallbackQuery, state: FSMContext):
-    await callback.message.answer('Попробуйте отправить голосовое сообщение еще раз')
+    await callback.message.answer("Попробуйте отправить голосовое сообщение еще раз")
     await state.set_state(States.input_voice)
 
 
-@router.callback_query(F.data == 'voice_good', States.voice_right_or_not)
+@router.callback_query(F.data == "voice_good", States.voice_right_or_not)
 async def voice_good(callback: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    to_do = data['to_do']
-    file_name = data['file_name']
+    to_do = data["to_do"]
+    file_name = data["file_name"]
 
-    if to_do == callback_data['all_type_actions']['report_traffic_accident']:
-        await traffic_accident(
-            callback=callback,
-            state=state
-        )
+    if to_do == callback_data["all_type_actions"]["report_traffic_accident"]:
+        await traffic_accident(callback=callback, state=state)
         await state.set_state(States.input_description_for_traffic_accident)
-    elif to_do == callback_data['all_type_actions']['report_road_deficiencies']:
-        await road_deficiencies(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["report_road_deficiencies"]:
+        await road_deficiencies(callback=callback, state=state)
         await state.set_state(States.output_text_for_road_deficiencies)
 
-    elif to_do == callback_data['all_type_actions']['report_illegal_actions']:
-        await illegal_actions(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["report_illegal_actions"]:
+        await illegal_actions(callback=callback, state=state)
         await state.set_state(States.input_description_for_illegal_actions)
-    elif to_do == callback_data['all_type_actions']['report_road_block']:
-        await road_block(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["report_road_block"]:
+        await road_block(callback=callback, state=state)
         await state.set_state(States.input_description_for_road_block)
-    elif to_do == callback_data['all_type_actions']['recognize_meal']:
-        await meal(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["recognize_meal"]:
+        await meal(callback=callback, state=state)
         await state.set_state(States.recognize)
-    elif to_do == callback_data['all_type_actions']['recognize_gas_station']:
-        await gas_station(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["recognize_gas_station"]:
+        await gas_station(callback=callback, state=state)
         await state.set_state(States.recognize)
-    elif to_do == callback_data['all_type_actions']['recognize_car_service']:
-        await car_service(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["recognize_car_service"]:
+        await car_service(callback=callback, state=state)
         await state.set_state(States.recognize)
-    elif to_do == callback_data['all_type_actions']['recognize_parking_lot']:
-        await parking_lot(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["recognize_parking_lot"]:
+        await parking_lot(callback=callback, state=state)
         await state.set_state(States.recognize)
-    elif to_do == callback_data['all_type_actions']['recognize_attractions']:
-        await attractions(
-            callback=callback,
-            state=state
-        )
+    elif to_do == callback_data["all_type_actions"]["recognize_attractions"]:
+        await attractions(callback=callback, state=state)
         await state.set_state(States.recognize)
-    elif to_do == callback_data['all_type_actions']['recognize_dangerous_situation']:
+    elif to_do == callback_data["all_type_actions"]["recognize_dangerous_situation"]:
         await dangerous_situation(callback)
         await state.set_state(States.report)
     else:
-        await callback.message.answer(text=mes_data['bad_situation'])
+        await callback.message.answer(text=mes_data["bad_situation"])
         await state.set_state(States.input_voice)
     remove(file_name)
     print(f"voice_good: Current state: {await state.get_state()}")
--- Files/request.py
+++ Files/request.py
@@ -1,5 +1,7 @@
+import json
 import requests
-domain = 'http://213.171.29.33:5139'
+
+domain = "http://213.171.29.33:5139"
 
 
 async def get_road_and_region(longitude: float, latitude: float):
@@ -9,14 +11,11 @@ async def get_road_and_region(longitude: float, latitude: float):
     :param latitude: float: ширина
     :return: {"roadName": "string", "regionName": "string"}
     """
-    url = f'{domain}/api/v1/TgBot/location'
-    data = {
-        'Coordinates.Longitude': longitude,
-        'Coordinates.Latitude': latitude
-    }
+    url = f"{domain}/api/v1/TgBot/location"
+    data = {"Coordinates.Longitude": longitude, "Coordinates.Latitude": latitude}
     response = requests.get(url, params=data)
     if response.status_code != 200:
-        print(f'Error {response.status_code}: {response.text}')
+        print(f"Error {response.status_code}: {response.text}")
         return
     return response.json()
 
@@ -31,7 +30,7 @@ async def get_request_audio(file_id: str):
     url = f"{domain}/api/v1/TgBot/advertisements/{file_id}/voice"
     response = requests.get(url)
     if response.status_code != 200:
-        print(f'Error {response.status_code}: {response.text}')
+        print(f"Error {response.status_code}: {response.text}")
     return response
 
 
@@ -42,12 +41,12 @@ async def get_advertisements_for_region(region_name: str):
     :param region_name: str: название региона, которое передается в url запроса
     :return: {"advertisements": [{"id": "uuid", "title": "string", "description": "string"}]}
     """
-    url = f'{domain}/api/v1/TgBot/regions/{region_name}/advertisements'
+    url = f"{domain}/api/v1/TgBot/regions/{region_name}/advertisements"
     print(url)
     response = requests.get(url)
     print(response)
     if response.status_code != 200:
-        print(f'Error {response.status_code}: {response.text}')
+        print(f"Error {response.status_code}: {response.text}")
         return
     return response.json()
 
@@ -59,22 +58,22 @@ async def get_request_urgent_message(road_name: str):
     :param road_name: str: название дороги, которое передается в url запроса:
     :return: {"advertisements": [{"id": "uuid", "title": "string", "description": "string"}]}
     """
-    url = f'{domain}/api/v1/TgBot/Roads/{road_name}/advertisements'
+    url = f"{domain}/api/v1/TgBot/Roads/{road_name}/advertisements"
     response = requests.get(url)
     if response.status_code != 200:
-        print(f'Error {response.status_code}: {response.text}')
+        print(f"Error {response.status_code}: {response.text}")
         return
     return response.json()
 
 
 # ветка "сообщить" ---------------------------------------------
 async def post_request_location_and_description(
-        road_name: str,
-        longitude: float,
-        latitude: float,
-        type_road: int,
-        description: str,
-        level: int
+    road_name: str,
+    longitude: float,
+    latitude: float,
+    type_road: int,
+    description: str,
+    level: int,
 ):
     """
     Cоздает новую не верифицированную точку
@@ -86,31 +85,24 @@ async def post_request_location_and_description(
     :param level: int: уровень доверия
     :return: {"pointId": str}
     """
-    url = f'{domain}/api/v1/TgBot/UnverifiedPoints'
+    url = f"{domain}/api/v1/TgBot/UnverifiedPoints"
     data = {
-        "coordinates": {
-            "latitude": latitude,
-            "longitude": longitude
-        },
+        "coordinates": {"latitude": latitude, "longitude": longitude},
         "type": type_road,
         "reliability": level,
         "description": description,
-        "roadName": road_name
+        "roadName": road_name,
     }
     print(data)
     response = requests.post(url, json=data)
     if response.status_code != 201:
-        print(f'Error {response.status_code}: {response.text}')
+        print(f"Error {response.status_code}: {response.text}")
         return False
     print(response.json())
     return response.json()
 
 
-async def post_request_media(
-        file_id: str,
-        point_id: str,
-        type_media: str
-) -> bool:
+async def post_request_media(file_id: str, point_id: str, type_media: str) -> bool:
     """
     Прикрепляет файл (фото или видео) к не вверифицированной точке
     :param file_id: id файла, берущееся в качестве названия для файла
@@ -118,9 +110,16 @@ async def post_request_media(
     :param type_media: расширение файла (.mp4 / .jpg)
     :return: True (в случае статус кода 201) / False (в случает какой либо ошибки)
     """
-    url = f'{domain}/api/v1/TgBot/UnverifiedPoints/{point_id}/file'
-    fp = open(f'{file_id}.{type_media}', 'rb')
-    files = {'file': (f'{file_id}.{type_media}', fp, 'image/jpeg' if type_media == 'jpg' else 'video/mp4', {})}
+    url = f"{domain}/api/v1/TgBot/UnverifiedPoints/{point_id}/file"
+    fp = open(f"{file_id}.{type_media}", "rb")
+    files = {
+        "file": (
+            f"{file_id}.{type_media}",
+            fp,
+            "image/jpeg" if type_media == "jpg" else "video/mp4",
+            {},
+        )
+    }
     response = requests.post(url, files=files)
     print(response)
     fp.close()
@@ -130,10 +129,7 @@ async def post_request_media(
 
 # ветка "узнать" --------------------------------------------------
 async def get_request_for_dots(
-        road_name: str,
-        longitude: float,
-        latitude: float,
-        point_type: str
+    road_name: str, longitude: float, latitude: float, point_type: str
 ):
     """
     Получение определенного кол-ва вер.точек на дороге в радиусе с типом
@@ -143,15 +139,28 @@ async def get_request_for_dots(
     :param point_type: тип отправляемой точки (Cafe, GasStation, CarService, RestPlace, InterestingPlace)
     :return: {"points": [{"name": str, "type": num, "coordinates": {"latitude": num, "longitude": num},"distanseFromUser": num}]}
     """
-    url = f'{domain}/api/v1/TgBot/Roads/{road_name}/verifiedPoints/{point_type}'
+    url = f"{domain}/api/v1/TgBot/Roads/{road_name}/verifiedPoints/{point_type}"
     data = {
-        'Coordinates.Longitude': longitude,
-        'Coordinates.Latitude': latitude,
-        'PointsCount': 10,
-        'Radius': 50
+        "Coordinates.Longitude": longitude,
+        "Coordinates.Latitude": latitude,
+        "PointsCount": 10,
+        "Radius": 50,
     }
     response = requests.get(url, params=data)
     if response.status_code != 200:
-        print(f'Error {response.status_code}: {response.text}')
+        print(f"Error {response.status_code}: {response.text}")
+        return
+    return response.json()
+
+
+# ветка "гс" --------------------------------------------------
+
+
+async def get_audio_label(path_to_file: str) -> json:
+    url = "http://127.0.0.1:8080/classify/audio/road"
+    file = {"file": open(path_to_file, "rb")}
+    response = requests.post(url, files=file)
+    if response.status_code != 200:
+        print(f"Error {response.status_code}: {response.text}")
         return
     return response.json()
